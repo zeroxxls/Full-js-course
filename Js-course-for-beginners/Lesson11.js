db4afaf9c654af5b383262639206911405cf6a8c
zeroxxl <zeroxxhls@gmail.com>
2024-06-02 09:13:00 +0200

    Lesson11

--- Js-course-for-beginners/Lesson11.js
+++ Js-course-for-beginners/Lesson11.js
@@ -7,3 +7,40 @@ const add1 = (a,b,callback) => callback(a + b);
 const res1 = add1(2,3,(res1)=>{
     console.log(res1)
 })
+
+//Контракты колбэков
+const add2 = (a,b,callback) =>{
+    if(!Number.isFinite(a) || !Number.isFinite(b)){
+        callback(new Error('Invalid arguments'));
+        return
+    }
+    callback(null,a+b)
+};
+
+const res2 =(2,3,(error,result) =>{
+    if(error)console.error(error);
+    console.log({result})
+})
+
+//Колбэки в методах массивах
+const stock = [
+    {name:'Laptop', quantity: 15,},
+    {name:'Pad', quantity: 0,},
+    {name:'Mouse', quantity: 10,},
+];
+const inStock = (item) => item.quantity > 0;
+const available = stock.filter(inStock);
+console.log(available)
+
+//Таймеры одноразовое событие
+setTimeout(() => {
+    console.log('#callback1');},
+    20);
+
+setTimeout(() => {
+    console.log('#callback2');},
+ 10);
+
+setTimeout(() => {
+    console.log('#callback3');},
+15);
