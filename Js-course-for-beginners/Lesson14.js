eeccdbd56b4eca51603b5a746a1ead2ef4fa29b6
zeroxxl <zeroxxhls@gmail.com>
2024-06-05 09:11:01 +0200

    Eventlisteners

--- Js-course-for-beginners/Lesson14.js
+++ Js-course-for-beginners/Lesson14.js
@@ -21,3 +21,20 @@ const elem2 = document.querySelector("input");
 elem2.addEventListener("input",(e)=>{
         console.log(e.target.value)
 })
+
+elem2.addEventListener("keypress",(e)=>{
+    console.log("keypressed")
+})
+
+elem2.addEventListener("keydown",(e)=>{
+    console.log("keydown")
+})
+
+elem2.addEventListener("keyup",(e)=>{
+    console.log("keyup")
+})
+
+const elem3 = document.querySelector("div");
+elem3.addEventListener("mousemove", ()=>{
+    console.log("moved")
+})
