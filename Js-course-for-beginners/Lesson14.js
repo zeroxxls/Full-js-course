337086aa9ba444e3ef4d06a1175a27253f87f631
zeroxxl <zeroxxhls@gmail.com>
2024-06-06 12:27:18 +0200

    Eventlisteners and Event

--- Js-course-for-beginners/Lesson14.js
+++ Js-course-for-beginners/Lesson14.js
@@ -1,65 +1,65 @@
 //События в браузере AddEventlistener
-// const elem = document.querySelector("div");
+const elem = document.querySelector("div");
 
-// elem.addEventListener("click",()=>{console.log("click")});
-// elem.addEventListener("dblclick", ()=>{console.log("dblclick")})
+elem.addEventListener("click",()=>{console.log("click")});
+elem.addEventListener("dblclick", ()=>{console.log("dblclick")})
 
-// elem.addEventListener("mousedown",()=>{console.log("mousedown")});
-// elem.addEventListener("mouseup",()=>{console.log("mouseup")});
+elem.addEventListener("mousedown",()=>{console.log("mousedown")});
+elem.addEventListener("mouseup",()=>{console.log("mouseup")});
 
-// elem.addEventListener("contextmenu",()=>{console.log("contextmenu")})
-// elem.addEventListener("wheel",()=>{console.log("wheel")})
+elem.addEventListener("contextmenu",()=>{console.log("contextmenu")})
+elem.addEventListener("wheel",()=>{console.log("wheel")})
 
-// //
+//
 
-// const element = document.querySelector("form");
-// element.addEventListener("submit", (e)=>{
-//     e.preventDefault();
-//     console.log("submit")
-// })
+const element = document.querySelector("form");
+element.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    console.log("submit")
+})
 
-// const elem2 = document.querySelector("input");
-// elem2.addEventListener("input",(e)=>{
-//         console.log(e.target.value)
-// })
+const elem2 = document.querySelector("input");
+elem2.addEventListener("input",(e)=>{
+        console.log(e.target.value)
+})
 
-// elem2.addEventListener("keypress",(e)=>{
-//     console.log("keypressed")
-// })
+elem2.addEventListener("keypress",(e)=>{
+    console.log("keypressed")
+})
 
-// elem2.addEventListener("keydown",(e)=>{
-//     console.log("keydown")
-// })
+elem2.addEventListener("keydown",(e)=>{
+    console.log("keydown")
+})
 
-// elem2.addEventListener("keyup",(e)=>{
-//     console.log("keyup")
-// })
+elem2.addEventListener("keyup",(e)=>{
+    console.log("keyup")
+})
 
-// const elem3 = document.querySelector("div");
-// elem3.addEventListener("mousemove", ()=>{
-//     console.log("moved")
-// })
-// //
-// const button = document.querySelector('.btn');
-// const buttonTwo = document.querySelector('.btn2');
-// function handleClick(){
-//     console.log('click')
-// }
+const elem3 = document.querySelector("div");
+elem3.addEventListener("mousemove", ()=>{
+    console.log("moved")
+})
+//
+const button = document.querySelector('.btn');
+const buttonTwo = document.querySelector('.btn2');
+function handleClick(){
+    console.log('click')
+}
 
-// button.addEventListener('click', handleClick);
-// buttonTwo.addEventListener('click',handleClick)
+button.addEventListener('click', handleClick);
+buttonTwo.addEventListener('click',handleClick)
 
-// buttonTwo.removeEventListener('click',handleClick)
+buttonTwo.removeEventListener('click',handleClick)
 
 
-// const buttons = document.querySelectorAll('.btn');
-// console.log(buttons)
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons)
 
-// buttons.forEach(button =>{
-//     button.addEventListener('click',function(){
-//         console.log('clicked')
-//     })
-// })
+buttons.forEach(button =>{
+    button.addEventListener('click',function(){
+        console.log('clicked')
+    })
+})
 
 //Event вспылтие и погружение
 const buttons = document.querySelectorAll('.btn');
