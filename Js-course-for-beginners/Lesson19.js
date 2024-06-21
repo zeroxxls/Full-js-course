e766befac6807bb4e6a79ea51f9f859863dee8c8
zeroxxl <zeroxxhls@gmail.com>
2024-06-21 11:31:19 +0200

    Error and Exception handling

--- Js-course-for-beginners/Lesson19.js
+++ Js-course-for-beginners/Lesson19.js
@@ -1,1 +1,20 @@
 //Error and exception handling
+//Throw exception
+// const calculateTotal = (order)=>{
+//     throw 'Always throws'
+// };
+
+// const prices = [100,10,1000];
+// console.log(calculateTotal(prices));
+
+//Handle exception
+const calculateTotal1 = (order)=>{
+    throw new Error ('Always throws') //class Error
+};
+
+try{
+    const prices = [100,10,1000];
+    console.log(calculateTotal1(prices));
+}catch (err){
+    console.error(err)
+}
