f5fb0d25bc3adbbe4e381c99065d866ae84ec63e
zeroxxl <zeroxxhls@gmail.com>
2024-06-22 10:59:15 +0200

    Errors try catch

--- Js-course-for-beginners/Lesson19.js
+++ Js-course-for-beginners/Lesson19.js
@@ -8,13 +8,26 @@
 // console.log(calculateTotal(prices));
 
 //Handle exception
-const calculateTotal1 = (order)=>{
-    throw new Error ('Always throws') //class Error
-};
+// const calculateTotal1 = (order)=>{
+//     throw new Error ('Always throws') //class Error
+// };
+
+// try{
+//     const prices = [100,10,1000];
+//     console.log(calculateTotal1(prices));
+// }catch (err){
+//     console.error(err)
+// }
 
 try{
-    const prices = [100,10,1000];
-    console.log(calculateTotal1(prices));
-}catch (err){
-    console.error(err)
+    console.log(1);
+    console.log(2);
+    const user = {};
+    console.log(user.name);
+    if(!user.name){
+        throw new Error("Fucking err")
+    }
+    console.log(3);
+}catch(error){
+    console.error('error is here',error)
 }
